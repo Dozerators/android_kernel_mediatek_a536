f021877bf46d36a123e27b88c9708cc3ee4aba79
Dozerators <paranyuk.ura@gmail.com>
2017-11-11 23:19:37 +0200

    3.10.108

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sat Nov 11 22:58:52 EET 2017"
+#define LINUX_COMPILE_BY "yuri"
+#define LINUX_COMPILE_HOST "Paranyuk"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.10.108-A536--g53d7370-dirty"
+#define BUILD_INFO ""
+#define BUILD_FINGERPRINT ""
